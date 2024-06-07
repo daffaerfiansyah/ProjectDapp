475e40bdd5745719c40f5a876b4f3ace434abb3f
DaffaErfiansyah <dapp@gmail.com>
2024-06-07 13:11:33 +0700

    Add html

---
+++ main.js
@@ -0,0 +1,29 @@
+// ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING
+
+window.onscroll = function() {headerShadow()};
+
+function headerShadow() {
+    const navHeader =document.getElementsByClassName("navbar");
+
+    if (document.body.scrollTop > 50 || document.documentElement.ScrollTop > 50) {
+        navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+        navHeader.style.height = "70px";
+        navHeader.style.lineHeight = "70px";
+    } else {
+        navHeader.style.boxShadow = "none";
+        navHeader.style.height = "90px";
+        navHeader.style.lineHeight = "90px";
+    }
+}
+
+// TYPING EFFECT //
+var typingEffect = new Typed(".typedtext",{
+    strings : ["Web Developer", "Web Designer", "Engineering"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay : 2000
+});
+
+ScrollReveal().reveal('.section', { delay: 500 });
+ScrollReveal().reveal('.card', { delay: 1000 });
